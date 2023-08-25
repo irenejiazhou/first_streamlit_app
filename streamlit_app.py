dee5eb5b852cca7ebecbed84b996f6612f0b1e67
Irene Zhou <57390524+irenejiazhou@users.noreply.github.com>
2023-08-25 10:03:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -9,3 +10,9 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#  pull the data into a dataframe
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# display the dataframe on the streamlit page
+streamlit.dataframe(my_fruit_list)
