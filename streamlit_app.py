ee912fd67b0605750303f288c040884b243224a1
Irene Zhou <57390524+irenejiazhou@users.noreply.github.com>
2023-08-27 22:41:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,13 @@ import pandas
 import requests # To display fruityvice API response
 import snowflake.connector # bring in some code from the snowflake library added (snowflake-connector-python)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
